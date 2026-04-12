23f1febea5057434058efa11a84543e3870308da
Prayag Bhatt <prayagbhatt2003@gmail.com>
2026-04-12 13:32:00 +0000

    [feat-socket]: handle error while creating socket

--- server.c
+++ server.c
@@ -2,11 +2,17 @@
 #include<sys/socket.h>
 
 int main(){
-    int tcp_socket = socket(
-        AF_INET,  // IPv4
+    int tcp_socket_fd = socket(
+        AF_INET,     // IPv4
         SOCK_STREAM, // TCP
         0 
     );
 
-    printf("%d\n", tcp_socket);
+    if(tcp_socket_fd == -1){
+        perror("Failed to create a socket, fd = -1\n");
+        return 0;
+    }
+
+    printf("Socket created successfully, fd = %d\n", tcp_socket_fd); 
+    return 0;
 }
