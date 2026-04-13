5158879df2e8497a0f5d570938e344066acab415
Prayag Bhatt <prayagbhatt2003@gmail.com>
2026-04-13 07:02:48 +0000

    [feat-client-connection]: implement client connection handling and improve socket management

--- server.c
+++ server.c
@@ -3,9 +3,48 @@
 #include <arpa/inet.h>
 #include <string.h>
 
+/**
+ * handle_client_connection: Process incoming client connection
+ * @client_socket: File descriptor of the connected client socket
+ *
+ * Receives data from the client socket and processes the request.
+ * Returns the number of bytes received, or -1 on error (ssize_t)
+ */
+ssize_t handle_client_connection(int client_socket)
+{
+    ssize_t n = 0;
+    char buffer[4096];
+
+    for (;;)
+    {
+        memset(buffer, 0, sizeof(buffer));
+
+        /* Receive incoming data from client socket */
+        n = read(client_socket, buffer, sizeof(buffer) - 1);
+        if (n < 0)
+        {
+            perror("Failed to read the data from the client socket\n");
+            return -1;
+        }
+        if (n == 0)
+        {
+            printf("Connection closed gracfully\n");
+            return 0;
+        }
+        printf(" - - - - - - - - - - - - - - - - - - - -\n");
+        printf("Buffer => %s\n", buffer);
+        printf(" - - - - - - - - - - - - - - - - - - - -\n");
+    }
+
+    return 0;
+}
+
+/**
+ * Creates a TCP socket, binds to port 8000, and accepts incoming connections.
+ * Returns 0 on success, 1 on error.
+ */
 int main()
 {
-    /* declaration */
     int ret = 0;
     int rc = 0;
     int tcp_socket = 0;
@@ -13,14 +52,10 @@ int main()
     int client_socket = 0;
     struct sockaddr_in bind_addr;
 
-    /* initialization */
     memset(&bind_addr, 0, sizeof(bind_addr));
 
-    // socket functions returns a FD
-    tcp_socket = socket(
-        AF_INET,     // IPv4
-        SOCK_STREAM, // TCP
-        0);
+    /* Create TCP socket (AF_INET=IPv4, SOCK_STREAM=TCP) */
+    tcp_socket = socket(AF_INET, SOCK_STREAM, 0);
 
     if (tcp_socket < 0)
     {
@@ -30,12 +65,10 @@ int main()
 
     printf("Socket created successfully, fd = %d\n", tcp_socket);
 
-    /*
-        The TCP Socket needs an address, so we use the bind() to connect to an address
-    */
-    bind_addr.sin_port = htons(8000);
-    bind_addr.sin_family = AF_INET;
-    bind_addr.sin_addr.s_addr = INADDR_ANY;
+    /* Configure socket address for binding to port 8000 */
+    bind_addr.sin_family = AF_INET;         /* IPv4 */
+    bind_addr.sin_port = htons(8000);       /* Port 8000 */
+    bind_addr.sin_addr.s_addr = INADDR_ANY; /* Listen on all interfaces */
 
     rc = bind(tcp_socket, (const struct sockaddr *)&bind_addr, sizeof(bind_addr));
     if (rc < 0)
@@ -45,9 +78,7 @@ int main()
         goto exit;
     }
 
-    /*
-        listen()  marks  the  socket  referred to by sockfd as a passive socket, that is, as a socket that will be used to accept incoming connection requests using accept(2)
-    */
+    /* Mark socket as passive to accept incoming connections */
     ls = listen(tcp_socket, SOMAXCONN);
     if (ls < 0)
     {
@@ -57,6 +88,7 @@ int main()
     }
     printf("Listener succeeded\n");
 
+    /* Accept and handle incoming client connections */
     for (;;)
     {
         printf("Waiting for connection\n");
@@ -68,6 +100,7 @@ int main()
             goto exit;
         }
         printf("Received a connection %d\n", client_socket);
+        rc = handle_client_connection(client_socket);
     }
 
 exit:
