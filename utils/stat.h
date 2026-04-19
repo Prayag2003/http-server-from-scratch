d8fac60ec5ebdc42df1b6178b34489c2aeac2d97
Prayag Bhatt <prayagbhatt2003@gmail.com>
2026-04-19 16:11:39 +0530

    [feat-stat]: add fs_get_metadata function to retrieve file existence status

---
+++ utils/stat.h
@@ -0,0 +1,32 @@
+#include <stdlib.h>
+#include <utils/string_ops.h>
+#include <linux/limits.h>
+#include <sys/stat.h>
+
+typedef struct
+{
+    bool exists;
+} fs_metadata;
+
+fs_metadata fs_get_metadata(string_view filename)
+{
+    char buf[PATH_MAX];
+    struct stat st;
+    fs_metadata metadata;
+    metadata.exists = false;
+
+    ssize_t len = filename.end - filename.start;
+    if (len <= 0 || len + 1 >= PATH_MAX)
+    {
+        return metadata;
+    }
+    memset(buf, 0, sizeof(buf));
+    memcpy(buf, filename.start, filename.end - filename.start);
+
+    if (stat(buf, &st) < 0)
+    {
+        return metadata;
+    }
+    metadata.exists = true;
+    return metadata;
+}
